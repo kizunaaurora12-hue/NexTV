3dc01ad38901a5dcf608956eb2712596469d3212
kizunaaurora12-hue <kizunaaurora12@gmail.com>
2026-03-15 00:29:39 +0800

    Add files via upload

---
+++ app/src/main/java/com/nextv/app/AppApplication.java
@@ -0,0 +1,11 @@
+package com.nextv.app;
+
+import android.app.Application;
+
+public class AppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
